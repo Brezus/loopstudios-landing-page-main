af9a799968a0c1b1820dc6d8c3bea6ed9bf25394
Roshane Miller <millerroshane44@gmail.com>
2022-01-10 16:03:11 -0500

    mobile first design
    mobile first approach with some js functionality

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const burger = document.querySelector(".burger");
+const hiddenNav = document.querySelector(".nav-ul");
+const header = document.querySelector(".header");
+const acomplishmentsGrid = document.querySelector(".sec2-grid");
+const hiddenAccsGrid = document.querySelectorAll(".acc");
+const seeAllBtn = document.querySelector(".mobile-btn");
+let showMorePressed = false;
+
+burger.addEventListener("click", () => {
+  hiddenNav.classList.toggle("active");
+  header.classList.toggle("active");
+  if (burger.srcset != "/images/icon-close.svg") {
+    burger.srcset = "/images/icon-close.svg";
+  } else {
+    burger.srcset = "/images/icon-hamburger.svg";
+  }
+});
+
+seeAllBtn.addEventListener("click", () => {
+  if (showMorePressed === false) {
+    acomplishmentsGrid.classList.add("show");
+    for (let i = 0; i < hiddenAccsGrid.length; i++) {
+      hiddenAccsGrid[i].classList.add("show-acomplishment");
+      hiddenAccsGrid[i].classList.remove("dp-none");
+    }
+    showMorePressed = true;
+  } else if (showMorePressed) {
+    acomplishmentsGrid.classList.remove("show");
+    for (let i = 0; i < hiddenAccsGrid.length; i++) {
+      hiddenAccsGrid[i].classList.remove("show-acomplishment");
+      hiddenAccsGrid[i].classList.add("dp-none");
+    }
+    showMorePressed = false;
+  }
+});
